7c98290d84ae2836b4d6893e2dc7658abb8ee116
matheusqb <matheus.mizael.ferreira@escola.pr.gov.br>
2025-09-04 10:58:13 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const avanca = Document.querySelectorAll('.btn-proximo');
 const reiniciarBtn = document.getElementById('btn-reiniciar');
 
-avanca.array.forEach(button=>
-    button.addEventListener('click') 
-);
+avanca.array.forEach(button=>{
+    button.addEventListener('click',function(){
+      const atual= document.querySelection('.ativo');
+      const poximoPasso ='passo-'+this.getAttribute('data-proximo');
+      
+     atual.classlist.remove('ativo');
+     const proximoElemento=document.getElementById(poximoPasso);
+
+     if (proximoElemento){
+        proximoElemento.classlist.add('ativo');
+     }else{
+     console.error(`elemento com ID "${poximoPasso}" não encontrado.`);
+     }
+    });
+});
